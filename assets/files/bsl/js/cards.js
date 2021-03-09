7c57da80c89b7aadac8f0e0972f5b87f51accf79
tedjh <ted.jh@hotmail.com>
2021-03-09 23:47:57 +0000

    add bsl again

---
+++ assets/files/bsl/js/cards.js
@@ -0,0 +1,42 @@
+flashcardDeck = {
+  title: "BSL",
+  catchPhrase: "Test how well you know British Sign Language",
+  cards: [
+    {
+      "question": "<img src='images/a.jpeg'",
+      "answer": "A,a.</sup>"
+    },
+    {
+      "question": "<img src='images/b.jpeg'",
+      "answer": "B,b.</sup>"
+    },
+    {
+      "question": "<img src='images/c.jpeg'",
+      "answer": "C,c.</sup>"
+    },
+    {
+      "question": "<img src='images/d.jpeg'",
+      "answer": "D,d.</sup>"
+    },
+    {
+      "question": "<img src='images/e.jpeg'",
+      "answer": "E,e.</sup>"
+    },
+    {
+      "question": "<img src='images/f.jpeg'",
+      "answer": "F,f.</sup>"
+    },
+    {
+      "question": "<img src='images/g.jpeg'",
+      "answer": "G,g.</sup>"
+    },
+    {
+      "question": "<img src='images/h.jpeg'",
+      "answer": "H,h.</sup>"
+    },
+    /*{
+      "question": "### What is meant by the statement \"some functions have side effects\"?",
+      "answer": "In computer science, a function or expression is said to have a side effect if, in addition to returning a value, it also modifies some state or has an observable interaction with calling functions or the outside world. For example, a function might modify a global variable or static variable, modify one of its arguments, raise an exception, write data to a display or file, read data, or call other side-effecting functions. In the presence of side effects, a program's behavior may depend on history; that is, the order of evaluation matters. Understanding and debugging a function with side effects requires knowledge about the context and its possible histories.||<sup>ref: [Wikipedia](http://en.wikipedia.org/wiki/Side_effect_%28computer_science%29)</sup>"
+    }*/
+  ]
+};
