f3c360c5c84f0884352f876341f4618317fed09f
tedjh <ted.jh@hotmail.com>
2021-03-11 16:43:55 +0000

    cards added back

--- assets/files/bsl/js/cards.js
+++ assets/files/bsl/js/cards.js
@@ -17,7 +17,7 @@ flashcardDeck = {
     {
       "question": "<img src='images/d.jpeg'>",
       "answer": "D</sup>"
-    }/*,
+    },
     {
       "question": "<img src='images/e.jpeg'>",
       "answer": "E</sup>"
@@ -106,6 +106,5 @@ flashcardDeck = {
       "question": "<img src='images/z.jpeg'>",
       "answer": "Z</sup>"
     }
-    */
   ]
 };
